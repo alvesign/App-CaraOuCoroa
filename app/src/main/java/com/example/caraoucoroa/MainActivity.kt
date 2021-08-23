f746e10d5fd92d1cb47e719dde640cf99d1b1ea9
Márcio Alves <marcio.webdesigner@gmail.com>
2021-08-22 22:53:52 -0300

    App Cara ou Coroa utilizando Kotlin para Android

---
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.caraoucoroa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image_jogar.setOnClickListener{
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+            //Gerar número randômico
+            val numero = Random().nextInt(2)
+
+            //Enviar número para a próxima activity
+            intent.putExtra("numero", numero)
+            startActivity(intent)
+        }
+    }
+}
